8a6eb6ebcdc4f909930b7ed9f66cdfbac456fbc1
akanksha-halde <haldeakanksha@yahoo.com>
2022-10-14 13:39:54 +0530

    Update CalcuteAgeOfPerson.py

--- CalcuteAgeOfPerson.py
+++ CalcuteAgeOfPerson.py
@@ -1,9 +1,11 @@
 #Calculate Age of a person
-
+#Datetime module is used
 import datetime as dt
+#date.today() tells us todays date
 current_date=dt.date.today()
 #print(type(x))
 def agecalculator(birthd):
+    #In here the comparison operation returns True/False and according to that if it id true 1 is taken and vice versa
     age=(current_date.year-birthd.year)-((current_date.month,current_date.day)<(birthd.month,birthd.day))   
     return age
     
